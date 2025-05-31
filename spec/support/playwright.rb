b1213017f12378a92580a45c9eb1a53a5652491c
elasticspoon <mail@yuribocharov.dev>
2025-05-31 19:37:42 -0400

    feat: add playwright and dummy data
    Adds playwright as a driver for system tests
    Scaffolds a dummy books MVC

---
+++ spec/support/playwright.rb
@@ -0,0 +1,14 @@
+require "capybara/rspec"
+require "playwright"
+
+Capybara.register_driver :my_playwright do |app|
+  Capybara::Playwright::Driver.new(app,
+    browser_type: ENV["PLAYWRIGHT_BROWSER"]&.to_sym || :chromium,
+    headless: (false unless ENV["CI"] || ENV["PLAYWRIGHT_HEADLESS"]))
+end
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :my_playwright
+  end
+end
