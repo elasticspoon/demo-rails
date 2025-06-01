8839d8d901c1467da53e9f627e94616793542993
elasticspoon <mail@yuribocharov.dev>
2025-05-31 23:34:05 -0400

    feat: add module to monkey-patch capybara + playwright

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -2,10 +2,34 @@ require 'rails_helper'
 require 'playwright/test'
 
 RSpec.describe "Books", type: :system do
-  include Playwright::Test::Matchers
   it "works" do
     visit root_path
 
     expect(page).to have_text("Books")
+  end
+
+  describe do
+    include Playwright::Test::Matchers
+
+    it "fails" do
+      visit root_path
+
+      Capybara.current_session.driver.with_playwright_page do |page|
+        expect(page.locator("html")).to have_text("Books")
+      end
+
+      aggregate_failures do
+        expect(page.find('body')).to have_text("Books")
+        within 'html' do
+          within 'body' do
+            expect(page).to have_text("Books")
+          end
+        end
+        within 'body' do
+          expect(page).to have_text("Books")
+        end
+        expect(page).to have_text("Books")
+      end
+    end
   end
 end
