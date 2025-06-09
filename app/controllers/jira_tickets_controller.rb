0613caa3f3827e04640748f24e27cd7676fce472
elasticspoon <mail@yuribocharov.dev>
2025-06-09 18:52:44 -0400

    feat: inline edits

--- app/controllers/jira_tickets_controller.rb
+++ app/controllers/jira_tickets_controller.rb
@@ -4,6 +4,40 @@ class JiraTicketsController < ApplicationController
     @tickets = @commit_metadatum.jira_tickets
   end
 
+  def edit
+    @ticket = JiraTicket.find(params[:id])
+    @commit_metadatum = @ticket.commit_metadata
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
+  end
+
+  def update
+    @ticket = JiraTicket.find(params[:id])
+    @commit_metadatum = @ticket.commit_metadata
+
+    if @ticket.update(ticket_params)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to commit_metadatum_jira_tickets_path(@commit_metadatum) }
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @ticket = JiraTicket.find(params[:id])
+    @commit_metadatum = @ticket.commit_metadata
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
+  end
+
   def destroy
     @ticket = JiraTicket.find(params[:id])
     @commit_metadatum = @ticket.commit_metadata
@@ -13,5 +47,11 @@ class JiraTicketsController < ApplicationController
       format.turbo_stream
       format.html { redirect_to commit_metadatum_jira_tickets_path(@commit_metadatum) }
     end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:jira_ticket).permit(:ticket_number)
   end
 end
