fbf3acd0ef8703fab4cf624e55233cfaf0e6d4ab
elasticspoon <mail@yuribocharov.dev>
2025-06-09 13:26:34 -0400

    refactor: Update Jira ticket parsing and model associations

--- app/models/jira_ticket.rb
+++ app/models/jira_ticket.rb
@@ -1,9 +1,8 @@
 class JiraTicket < ApplicationRecord
-  belongs_to :commit_metadatum
-  
-  validates :ticket_number, presence: true, 
-    format: { with: /\A[A-Z]+-\d+\z/, message: "must be in format ABC-123" }
-    
+  belongs_to :commit_metadata
+
+  validates :ticket_number, presence: true
+
   def url
     "https://jira.example.com/browse/#{ticket_number}"
   end
